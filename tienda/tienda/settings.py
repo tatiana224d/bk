cc756ebc0e608c62d81bbd988817a52eaacf75f3
tatiana224d <heidy.caicedo@inacapmail.cl>
2025-11-23 19:12:52 -0300

    Sintaxis settings
    Corregi el lenguaje del admin y el time zone

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'america/Santiago'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
