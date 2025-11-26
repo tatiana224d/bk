2370202329c7d1c04b1b1b4f7d2db7e1b0276c97
francisca <francisca.fernandez120@gmail.com>
2025-11-26 11:46:47 -0300

    creacion de model catalogo

--- tienda/online/admin.py
+++ tienda/online/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from online.models import pedidos
+from online.models import pedidos, catalogo
 # Register your models here.
 
 class pedidosAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'email', 'telefono', 'descripcion', 'producto_ref', 'recibido_de', 'fecha']
 
 admin.site.register(pedidos, pedidosAdmin)
+
+@admin.register(catalogo)
+class catalogoAdmin(admin.ModelAdmin):
+    list_display = ['nombre_producto', 'precio_producto']
+
--- tienda/online/models.py
+++ tienda/online/models.py
@@ -12,6 +12,14 @@ class pedidos(models.Model):
     recibido_de = models.CharField(max_length=120) #wsp, o presencial
 
     fecha=models.DateField() #fecha para la que se necesita el producto
+    def __str__(self):
+        return self.nombre
+    
+class catalogo(models.Model):
+    nombre_producto = models.CharField(max_length=120)
+    descripcion_producto = models.TextField(max_length=250)
+    imagen_producto = models.ImageField(upload_to='product_images/')
+    precio_producto = models.DecimalField(max_digits=10, decimal_places=2)
 
-
-
+    def __str__(self):
+        return self.nombre_producto
